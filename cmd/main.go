f998180a8a08aa713fb5d000d6b3e5fbf363ed09
Devendra Pratap <devendra.pratap@hostbooks.com>
2025-11-03 12:02:38 +0530

    main changes

--- cmd/main.go
+++ cmd/main.go
@@ -13,31 +13,6 @@ import (
 
 func main() {
 	// Initialize via wire (generated wire_gen.go)
-	// restHandler, grpcServer, err := wire.InitializeApp()
-	// if err != nil {
-	// 	log.Fatalf("failed to initialize: %v", err)
-	// }
-	// // Start REST
-	// go func() {
-	// 	app := fiber.New()
-	// 	restHandler.Register(app)
-	// 	log.Println("REST listening :8080")
-	// 	if err := app.Listen(":8080"); err != nil {
-	// 		log.Fatalf("REST server error: %v", err)
-	// 	}
-	// }()
-	// // Start gRPC
-	// lis, err := net.Listen("tcp", ":50051")
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	// s := grpc.NewServer()
-	// pb.RegisterCryptoServiceServer(s, grpcServer)
-	// log.Println("gRPC listening :50051")
-	// if err := s.Serve(lis); err != nil {
-	// 	log.Fatalf("gRPC serve error: %v", err)
-	// }
-
 	appHandlers, err := wire.InitializeApp()
 	if err != nil {
 		log.Fatalf("failed to init app: %v", err)
